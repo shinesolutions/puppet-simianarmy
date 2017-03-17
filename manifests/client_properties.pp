22c65045132e18662928368e1b51d90ee3c4a434
James Sinclair <james.sinclair@shinesolutions.com>
2017-03-17 16:51:16 +1100

    Update client_properties class.

--- manifests/client_properties.pp
+++ manifests/client_properties.pp
@@ -18,11 +18,56 @@
 # [*mode*]
 #   The mode of the application.properties file. Passed to the `file` resource.
 #
+# [*simianarmy_client_aws_account_key*]
+#   Java property `simianarmy.client.aws.accountKey`
+#
 # [*simianarmy_client_aws_account_name*]
 #   Java property `simianarmy.client.aws.accountName`
 #
+# [*simianarmy_client_aws_assume_role_arn*]
+#   Java property `simianarmy.client.aws.assumeRoleArn`
+#
+# [*simianarmy_client_aws_proxy_host*]
+#   Java property `simianarmy.client.aws.proxyHost`
+#
+# [*simianarmy_client_aws_proxy_password*]
+#   Java property `simianarmy.client.aws.proxyPassword`
+#
+# [*simianarmy_client_aws_proxy_port*]
+#   Java property `simianarmy.client.aws.proxyPort`
+#
+# [*simianarmy_client_aws_proxy_user*]
+#   Java property `simianarmy.client.aws.proxyUser`
+#
 # [*simianarmy_client_aws_region*]
 #   Java property `simianarmy.client.aws.region`
+#
+# [*simianarmy_client_aws_secret_key*]
+#   Java property `simianarmy.client.aws.secretKey`
+#
+# [*simianarmy_client_chaos_class*]
+#   Java property `simianarmy.client.chaos.class`
+#
+# [*simianarmy_client_context_class*]
+#   Java property `simianarmy.client.context.class`
+#
+# [*simianarmy_client_recorder_class*]
+#   Java property `simianarmy.client.recorder.class`
+#
+# [*simianarmy_client_vsphere_password*]
+#   Java property `simianarmy.client.vsphere.password`
+#
+# [*simianarmy_client_vsphere_termination_strategy_property_name*]
+#   Java property `simianarmy.client.vsphere.terminationStrategy.property.name`
+#
+# [*simianarmy_client_vsphere_termination_strategy_property_value*]
+#   Java property `simianarmy.client.vsphere.terminationStrategy.property.value`
+#
+# [*simianarmy_client_vsphere_url*]
+#   Java property `simianarmy.client.vsphere.url`
+#
+# [*simianarmy_client_vsphere_username*]
+#   Java property `simianarmy.client.vsphere.username`
 #
 # === Examples
 #
@@ -43,8 +88,23 @@ class simianarmy::client_properties (
   $group,
   $mode,
 
+  $simianarmy_client_aws_account_key = undef,
   $simianarmy_client_aws_account_name = undef,
+  $simianarmy_client_aws_assume_role_arn = undef,
+  $simianarmy_client_aws_proxy_host = undef,
+  $simianarmy_client_aws_proxy_password = undef,
+  $simianarmy_client_aws_proxy_port = undef,
+  $simianarmy_client_aws_proxy_user = undef,
   $simianarmy_client_aws_region = undef,
+  $simianarmy_client_aws_secret_key = undef,
+  $simianarmy_client_chaos_class = undef,
+  $simianarmy_client_context_class = undef,
+  $simianarmy_client_recorder_class = undef,
+  $simianarmy_client_vsphere_password = undef,
+  $simianarmy_client_vsphere_termination_strategy_property_name = undef,
+  $simianarmy_client_vsphere_termination_strategy_property_value = undef,
+  $simianarmy_client_vsphere_url = undef,
+  $simianarmy_client_vsphere_username = undef,
 
 ) {
   file { $path:
--- spec/classes/client_properties_spec.rb
+++ spec/classes/client_properties_spec.rb
@@ -3,12 +3,72 @@ describe 'simianarmy::client_properties' do
   context 'with defaults for all parameters' do
     it { is_expected.to contain_class('simianarmy::client_properties') }
   end
+  context 'with simianarmy_client_aws_account_key => foo' do
+    let(:params) { {:simianarmy_client_aws_account_key => 'foo', :path => '/tmp/foo', } }
+    it { is_expected.to contain_file('/tmp/foo').with_content(/^simianarmy.client.aws.accountKey = foo/) }
+  end
   context 'with simianarmy_client_aws_account_name => foo' do
     let(:params) { {:simianarmy_client_aws_account_name => 'foo', :path => '/tmp/foo', } }
     it { is_expected.to contain_file('/tmp/foo').with_content(/^simianarmy.client.aws.accountName = foo/) }
   end
+  context 'with simianarmy_client_aws_assume_role_arn => foo' do
+    let(:params) { {:simianarmy_client_aws_assume_role_arn => 'foo', :path => '/tmp/foo', } }
+    it { is_expected.to contain_file('/tmp/foo').with_content(/^simianarmy.client.aws.assumeRoleArn = foo/) }
+  end
+  context 'with simianarmy_client_aws_proxy_host => foo' do
+    let(:params) { {:simianarmy_client_aws_proxy_host => 'foo', :path => '/tmp/foo', } }
+    it { is_expected.to contain_file('/tmp/foo').with_content(/^simianarmy.client.aws.proxyHost = foo/) }
+  end
+  context 'with simianarmy_client_aws_proxy_password => foo' do
+    let(:params) { {:simianarmy_client_aws_proxy_password => 'foo', :path => '/tmp/foo', } }
+    it { is_expected.to contain_file('/tmp/foo').with_content(/^simianarmy.client.aws.proxyPassword = foo/) }
+  end
+  context 'with simianarmy_client_aws_proxy_port => foo' do
+    let(:params) { {:simianarmy_client_aws_proxy_port => 'foo', :path => '/tmp/foo', } }
+    it { is_expected.to contain_file('/tmp/foo').with_content(/^simianarmy.client.aws.proxyPort = foo/) }
+  end
+  context 'with simianarmy_client_aws_proxy_user => foo' do
+    let(:params) { {:simianarmy_client_aws_proxy_user => 'foo', :path => '/tmp/foo', } }
+    it { is_expected.to contain_file('/tmp/foo').with_content(/^simianarmy.client.aws.proxyUser = foo/) }
+  end
   context 'with simianarmy_client_aws_region => foo' do
     let(:params) { {:simianarmy_client_aws_region => 'foo', :path => '/tmp/foo', } }
     it { is_expected.to contain_file('/tmp/foo').with_content(/^simianarmy.client.aws.region = foo/) }
+  end
+  context 'with simianarmy_client_aws_secret_key => foo' do
+    let(:params) { {:simianarmy_client_aws_secret_key => 'foo', :path => '/tmp/foo', } }
+    it { is_expected.to contain_file('/tmp/foo').with_content(/^simianarmy.client.aws.secretKey = foo/) }
+  end
+  context 'with simianarmy_client_chaos_class => foo' do
+    let(:params) { {:simianarmy_client_chaos_class => 'foo', :path => '/tmp/foo', } }
+    it { is_expected.to contain_file('/tmp/foo').with_content(/^simianarmy.client.chaos.class = foo/) }
+  end
+  context 'with simianarmy_client_context_class => foo' do
+    let(:params) { {:simianarmy_client_context_class => 'foo', :path => '/tmp/foo', } }
+    it { is_expected.to contain_file('/tmp/foo').with_content(/^simianarmy.client.context.class = foo/) }
+  end
+  context 'with simianarmy_client_recorder_class => foo' do
+    let(:params) { {:simianarmy_client_recorder_class => 'foo', :path => '/tmp/foo', } }
+    it { is_expected.to contain_file('/tmp/foo').with_content(/^simianarmy.client.recorder.class = foo/) }
+  end
+  context 'with simianarmy_client_vsphere_password => foo' do
+    let(:params) { {:simianarmy_client_vsphere_password => 'foo', :path => '/tmp/foo', } }
+    it { is_expected.to contain_file('/tmp/foo').with_content(/^simianarmy.client.vsphere.password = foo/) }
+  end
+  context 'with simianarmy_client_vsphere_termination_strategy_property_name => foo' do
+    let(:params) { {:simianarmy_client_vsphere_termination_strategy_property_name => 'foo', :path => '/tmp/foo', } }
+    it { is_expected.to contain_file('/tmp/foo').with_content(/^simianarmy.client.vsphere.terminationStrategy.property.name = foo/) }
+  end
+  context 'with simianarmy_client_vsphere_termination_strategy_property_value => foo' do
+    let(:params) { {:simianarmy_client_vsphere_termination_strategy_property_value => 'foo', :path => '/tmp/foo', } }
+    it { is_expected.to contain_file('/tmp/foo').with_content(/^simianarmy.client.vsphere.terminationStrategy.property.value = foo/) }
+  end
+  context 'with simianarmy_client_vsphere_url => foo' do
+    let(:params) { {:simianarmy_client_vsphere_url => 'foo', :path => '/tmp/foo', } }
+    it { is_expected.to contain_file('/tmp/foo').with_content(/^simianarmy.client.vsphere.url = foo/) }
+  end
+  context 'with simianarmy_client_vsphere_username => foo' do
+    let(:params) { {:simianarmy_client_vsphere_username => 'foo', :path => '/tmp/foo', } }
+    it { is_expected.to contain_file('/tmp/foo').with_content(/^simianarmy.client.vsphere.username = foo/) }
   end
 end
