5a4348e528b234c071583fb9b3399cf8760bb783
James Sinclair <james.sinclair@shinesolutions.com>
2017-03-29 11:14:57 +1100

    Add failing tests for a missing `path` parameter.

--- spec/defines/chaos_properties_asg_spec.rb
+++ spec/defines/chaos_properties_asg_spec.rb
@@ -1,12 +1,17 @@
 require 'spec_helper'
 describe 'simianarmy::chaos_properties::asg' do
+  path = '/tmp/file.properties'
+
   let(:title) { 'test-asg' }
   let(:params) {{
-    :path => '/tmp/file.properties',
+    :path => path,
   }}
   context 'with defaults for all parameters' do
     it { is_expected.to contain_simianarmy__chaos_properties__asg('test-asg') }
-    it { is_expected.to contain_file_line('test-asg.enabled').with_line('simianarmy.chaos.ASG.test-asg.enabled = false') }
+    it { is_expected.to contain_file_line('test-asg.enabled')
+         .with_line('simianarmy.chaos.ASG.test-asg.enabled = false')
+         .with_path(path)
+    }
     it { is_expected.not_to contain_file_line('test-asg.probability') }
     it { is_expected.not_to contain_file_line('test-asg.maxTerminationsPerDay') }
     it { is_expected.not_to contain_file_line('test-asg.notification.enabled') }
@@ -14,38 +19,59 @@ describe 'simianarmy::chaos_properties::asg' do
   end
   context 'with enabled => true' do
     let(:params) {{
-      :path    => '/tmp/file.properties',
+      :path    => path,
       :enabled => true,
     }}
     it { is_expected.to contain_simianarmy__chaos_properties__asg('test-asg') }
-    it { is_expected.to contain_file_line('test-asg.enabled').with_line('simianarmy.chaos.ASG.test-asg.enabled = true') }
-    it { is_expected.to contain_file_line('test-asg.probability').with_line('simianarmy.chaos.ASG.test-asg.probability = 1.0') }
-    it { is_expected.to contain_file_line('test-asg.maxTerminationsPerDay').with_line('simianarmy.chaos.ASG.test-asg.maxTerminationsPerDay = 1.0') }
+    it { is_expected.to contain_file_line('test-asg.enabled')
+         .with_line('simianarmy.chaos.ASG.test-asg.enabled = true')
+         .with_path(path)
+    }
+    it { is_expected.to contain_file_line('test-asg.probability')
+         .with_line('simianarmy.chaos.ASG.test-asg.probability = 1.0')
+         .with_path(path)
+    }
+    it { is_expected.to contain_file_line('test-asg.maxTerminationsPerDay')
+         .with_line('simianarmy.chaos.ASG.test-asg.maxTerminationsPerDay = 1.0')
+         .with_path(path)
+    }
     it { is_expected.not_to contain_file_line('test-asg.notification.enabled') }
     it { is_expected.not_to contain_file_line('test-asg.ownerEmail') }
   end
   context 'with enabled => true and different probabilities' do
     let(:params) {{
-      :path                     => '/tmp/file.properties',
+      :path                     => path,
       :enabled                  => true,
       :probability              => '0.5',
       :max_terminations_per_day => '0.5',
     }}
     it { is_expected.to contain_simianarmy__chaos_properties__asg('test-asg') }
-    it { is_expected.to contain_file_line('test-asg.enabled').with_line('simianarmy.chaos.ASG.test-asg.enabled = true') }
-    it { is_expected.to contain_file_line('test-asg.probability').with_line('simianarmy.chaos.ASG.test-asg.probability = 0.5') }
-    it { is_expected.to contain_file_line('test-asg.maxTerminationsPerDay').with_line('simianarmy.chaos.ASG.test-asg.maxTerminationsPerDay = 0.5') }
+    it { is_expected.to contain_file_line('test-asg.enabled')
+         .with_line('simianarmy.chaos.ASG.test-asg.enabled = true')
+         .with_path(path)
+    }
+    it { is_expected.to contain_file_line('test-asg.probability')
+         .with_line('simianarmy.chaos.ASG.test-asg.probability = 0.5')
+         .with_path(path)
+    }
+    it { is_expected.to contain_file_line('test-asg.maxTerminationsPerDay')
+         .with_line('simianarmy.chaos.ASG.test-asg.maxTerminationsPerDay = 0.5')
+         .with_path(path)
+    }
     it { is_expected.not_to contain_file_line('test-asg.notification.enabled') }
     it { is_expected.not_to contain_file_line('test-asg.ownerEmail') }
   end
   context 'with enabled => false and owner_email => foo@bar.com.au' do
     let(:params) {{
-      :path        => '/tmp/file.properties',
+      :path        => path,
       :enabled     => false,
       :owner_email => 'foo@bar.com.au',
     }}
     it { is_expected.to contain_simianarmy__chaos_properties__asg('test-asg') }
-    it { is_expected.to contain_file_line('test-asg.enabled').with_line('simianarmy.chaos.ASG.test-asg.enabled = false') }
+    it { is_expected.to contain_file_line('test-asg.enabled')
+         .with_line('simianarmy.chaos.ASG.test-asg.enabled = false')
+         .with_path(path)
+    }
     it { is_expected.not_to contain_file_line('test-asg.probability') }
     it { is_expected.not_to contain_file_line('test-asg.maxTerminationsPerDay') }
     it { is_expected.not_to contain_file_line('test-asg.notification.enabled') }
@@ -53,15 +79,30 @@ describe 'simianarmy::chaos_properties::asg' do
   end
   context 'with enabled => true and owner_email => foo@bar.com.au' do
     let(:params) {{
-      :path        => '/tmp/file.properties',
+      :path        => path,
       :enabled     => true,
       :owner_email => 'foo@bar.com.au',
     }}
     it { is_expected.to contain_simianarmy__chaos_properties__asg('test-asg') }
-    it { is_expected.to contain_file_line('test-asg.enabled').with_line('simianarmy.chaos.ASG.test-asg.enabled = true') }
-    it { is_expected.to contain_file_line('test-asg.probability').with_line('simianarmy.chaos.ASG.test-asg.probability = 1.0') }
-    it { is_expected.to contain_file_line('test-asg.maxTerminationsPerDay').with_line('simianarmy.chaos.ASG.test-asg.maxTerminationsPerDay = 1.0') }
-    it { is_expected.to contain_file_line('test-asg.notification.enabled').with_line('simianarmy.chaos.ASG.test-asg.notification.enabled = true') }
-    it { is_expected.to contain_file_line('test-asg.ownerEmail').with_line('simianarmy.chaos.ASG.test-asg.ownerEmail = foo@bar.com.au') }
+    it { is_expected.to contain_file_line('test-asg.enabled')
+         .with_line('simianarmy.chaos.ASG.test-asg.enabled = true')
+         .with_path(path)
+    }
+    it { is_expected.to contain_file_line('test-asg.probability')
+         .with_line('simianarmy.chaos.ASG.test-asg.probability = 1.0')
+         .with_path(path)
+    }
+    it { is_expected.to contain_file_line('test-asg.maxTerminationsPerDay')
+         .with_line('simianarmy.chaos.ASG.test-asg.maxTerminationsPerDay = 1.0')
+         .with_path(path)
+    }
+    it { is_expected.to contain_file_line('test-asg.notification.enabled')
+         .with_line('simianarmy.chaos.ASG.test-asg.notification.enabled = true')
+         .with_path(path)
+    }
+    it { is_expected.to contain_file_line('test-asg.ownerEmail')
+         .with_line('simianarmy.chaos.ASG.test-asg.ownerEmail = foo@bar.com.au')
+         .with_path(path)
+    }
   end
 end
